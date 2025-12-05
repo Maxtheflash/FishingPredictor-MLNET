44a8f53ddee3030b2b8237e2d0051f4b37c2a7a4
Branden Maxwell <maxtheflash@yahoo.com>
2025-12-05 17:09:45 -0500

    Add files via upload

---
+++ FishingPredictor-MLNET/FishingLiteData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+
+public class FishingLiteData
+{
+    [LoadColumn(0)]
+    public string? BaitType { get; set; }
+
+    [LoadColumn(1)]
+    public string? CatchSuccess { get; set; }
+}
+
+public class FishingLitePrediction
+{
+    [ColumnName("PredictedLabel")]
+    public string? CatchSuccess;
+}
---
+++ FishingPredictor-MLNET/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var mlContext = new MLContext();
+
+        // Load data
+        var data = mlContext.Data.LoadFromTextFile<FishingLiteData>("simple_data.csv", separatorChar: ',', hasHeader: true);
+
+        // Build pipeline
+        var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label", nameof(FishingLiteData.CatchSuccess))
+            .Append(mlContext.Transforms.Categorical.OneHotEncoding("BaitType"))
+            .Append(mlContext.Transforms.Concatenate("Features", "BaitType"))
+            .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+            .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+        // Train
+        var model = pipeline.Fit(data);
+
+        // Predict
+        var predictor = mlContext.Model.CreatePredictionEngine<FishingLiteData, FishingLitePrediction>(model);
+
+        var test = new FishingLiteData { BaitType = "Worm" };
+        var result = predictor.Predict(test);
+
+        Console.WriteLine($" your prediction today for bait '{test.BaitType}': {result.CatchSuccess}");
+        Console.ReadKey();
+    }
+}
